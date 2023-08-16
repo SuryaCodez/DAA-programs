866aae4059833f0a88096927c2136c6f19c688c5
surya kuruvadi <107850687+SuryaCodez@users.noreply.github.com>
2023-08-16 10:01:51 +0530

    Update quick sort with time complexity.cpp

--- quick sort with time complexity.cpp
+++ quick sort with time complexity.cpp
@@ -1,55 +1,59 @@
-#include <iostream>
+#include<iostream>
+#include<ctime>
+#define MAX 20
+
 using namespace std;
 
-void swap(int arr[] , int pos1, int pos2){
-	int temp;
-	temp = arr[pos1];
-	arr[pos1] = arr[pos2];
-	arr[pos2] = temp;
+int partition(int arr[], int low, int high, int pivot)
+{
+    int i = low;
+    int j = low;
+    
+    while(i<=high)
+    {
+        if(arr[i]>pivot)
+            i++;
+        else
+        {
+            int temp = arr[j];
+            arr[j] = arr[i];
+            arr[i] = temp;
+            i++;
+            j++;
+        }
+    }
+    return j-1;
 }
 
-int partition(int arr[], int low, int high, int pivot){
-	int i = low;
-	int j = low;
-	while( i <= high){
-		if(arr[i] > pivot){
-			i++;
-		}
-		else{
-			swap(arr,i,j);
-			i++;
-			j++;
-		}
-	}
-	return j-1;
-}
-
-void quickSort(int arr[], int low, int high){
-	if(low < high){
-	int pivot = arr[high];
-	int pos = partition(arr, low, high, pivot);
-	
-	quickSort(arr, low, pos-1);
-	quickSort(arr, pos+1, high);
-	}
+void quickSort(int arr[], int low, int high)
+{
+    int pivot, pos;
+    if(low<high)
+    {
+        pivot = arr[high];
+        pos = partition(arr, low, high, pivot);
+        quickSort(arr, low, pos-1);
+        quickSort(arr, pos+1, high);
+    }
 }
 
 int main()
 {
-    clock_t start,end;
-	int n ;
-	cout << " enter the size of array:";
-	cin>>n;
-	int arr[n];
-	for( int i = 0 ; i < n; i++){
-		cin>> arr[i];
-	}
-	quickSort(arr, 0 , n-1);
-	start=clock();
-	cout<<"The sorted array is: ";
-	for( int i = 0 ; i < n; i++){
-		cout<< arr[i]<<"\t";
-	}
-	end=clock();
-	cout<<"the time taken for quick sort is:\n"<<(double)(end-start)/CLOCKS_PER_SEC<<endl;
+    int arr[MAX], size;
+    cout<<"Enter size of array: ";
+    cin>>size;
+    cout<<"Enter elements: "<<endl;
+    for (int i=0;i<size;i++)
+        cin>>arr[i];
+    clock_t start = clock();
+    quickSort(arr, 0, size-1);
+    clock_t end = clock();
+    cout<<"printing array: "<<endl;
+    for(int i=0;i<size;i++)
+        cout<<arr[i]<<endl;
+    double time = double(end-start)/CLOCKS_PER_SEC;
+    cout<<"time taken: "<<time*1000000<<" Microsseconds\n";
+    return 0;
 }
+
+
